c4216f7408f2e14b6e35c4d1074855a9e89745f0
Kevin Sheurs <ksheurs@gmail.com>
2017-10-26 16:46:08 -0400

    First commit

---
+++ lib/peerage_ec2.ex
@@ -0,0 +1,18 @@
+defmodule PeerageEc2 do
+  @moduledoc """
+  Documentation for PeerageEc2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PeerageEc2.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/peerage_ec2_test.exs
@@ -0,0 +1,8 @@
+defmodule PeerageEc2Test do
+  use ExUnit.Case
+  doctest PeerageEc2
+
+  test "greets the world" do
+    assert PeerageEc2.hello() == :world
+  end
+end
