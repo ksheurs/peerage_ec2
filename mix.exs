0068958e532c105da8af3082419b9c98414ca895
Kevin Sheurs <ksheurs@gmail.com>
2017-10-30 14:30:45 -0400

    Add missing project config

--- mix.exs
+++ mix.exs
@@ -2,16 +2,17 @@ defmodule Peerage.Via.Ec2.Mixfile do
   use Mix.Project
 
   def project do
-    [
-      app: :peerage_ec2,
-      version: "1.0.0",
-      elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
-      deps: deps(),
-      name: "Peerage EC2",
-      description: description(),
-      source_url: "https://github.com/BoweryFarming/peerage_ec2"
-    ]
+    [app: :peerage_ec2,
+     version: "1.0.0",
+     elixir: "~> 1.5",
+     start_permanent: Mix.env == :prod,
+     deps: deps(),
+     name: "Peerage EC2",
+     package: package(),
+     description: description(),
+     source_url: "https://github.com/BoweryFarming/peerage_ec2",
+     homepage_url: "https://github.com/BoweryFarming/peerage_ec2",
+     docs: [main: "readme", extras: ["README.md"]]]
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -32,7 +33,8 @@ defmodule Peerage.Via.Ec2.Mixfile do
   end
 
   def package do
-    [maintainers: ["Kevin Sheurs", "Dylan Fareed", "Henry Sztul"],
+    [files: ["lib", "mix.exs", "README*"],
+     maintainers: ["Kevin Sheurs", "Dylan Fareed", "Henry Sztul"],
      licenses: ["MIT"],
      links: %{"GitHub" => "https://github.com/BoweryFarming/peerage_ec2",
               "Docs" => "https://hexdocs.pm/peerage_ec2/readme.html"}]
